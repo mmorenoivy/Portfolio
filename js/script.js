55f7e5664d3c990e97f0091298955944a53799c6
mmorenoivy <marjorie_moreno@hotmail.com>
2018-01-02 13:09:05 -0600

    added hero.jpg

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2018 00:00:00").getTime();
+var countDownDate = new Date("Feb 1, 2018 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -30,6 +30,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "Happy New Year 2018";
   }
 }, 1000);
